8740fb4262c1f75425737f66103d41cb51faae79
AlfredAgedePraiseO <150625873+AlfredAgedePraiseO@users.noreply.github.com>
2024-06-07 17:58:02 +0100

    Children-Account

---
+++ Children-Account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, account_number,account_name, balance=0):
+        self.account_number = account_number
+        self.account_name = account_name
+        self.balance = balance
+
+        def deposit(self, amount):
+            self.balance += amount
+
+            def withdraw(self, amount):
+                self.amount
+
+
+
+
